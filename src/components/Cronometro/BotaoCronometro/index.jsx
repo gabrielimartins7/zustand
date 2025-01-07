c0769d5b3772c90ac067981dceba0783b977b72c
gabrielimartins7 <martins.gabrieli.07.almeida@outlook.com>
2025-01-07 15:50:54 -0300

    implementando o estado global no timer e formatando o tempo do cronometro

--- src/components/Cronometro/BotaoCronometro/index.jsx
+++ src/components/Cronometro/BotaoCronometro/index.jsx
@@ -1,16 +1,27 @@
+import { useCronometroStore } from "../../../store";
+
 import styles from "./styles.module.css";
 import play_arrowImg from "/src/assets/imgs/play_arrow.png";
 import pauseImg from "/src/assets/imgs/pause.png";
 import audioPlaySom from "/src/assets/sons/play.wav";
 import audioPauseSom from "/src/assets/sons/pause.mp3";
 
-
 export default function BotaoCronometro() {
+  const intervaloId = useCronometroStore((estado) => estado.intervaloId);
+  const iniciarCronometro = useCronometroStore((estado) => estado.iniciarCronometro);
+
+  const textoIniciarOuPausar = intervaloId ? "Pausar" : "Começar";
+  const iconeIniciarOuPausar = intervaloId ? pauseImg : play_arrowImg;
+
   return (
     <div className={styles["cronometer__primary-button-wrapper"]}>
-      <button className={styles["cronometer__primary-button"]}>
-        <img className={styles["cronometer__primary-button-icon"]} src={play_arrowImg} alt="" />
-        <span>Começar</span>
+      <button className={styles["cronometer__primary-button"]} onClick={iniciarCronometro}>
+        <img
+          className={styles["cronometer__primary-button-icon"]}
+          src={iconeIniciarOuPausar}
+          alt=""
+        />
+        <span>{textoIniciarOuPausar}</span>
       </button>
     </div>
   );
--- src/store/index.jsx
+++ src/store/index.jsx
@@ -8,27 +8,55 @@ export const MODO_CRONOMETRO = {
         tempoInicialEmSegundos: 30,
     },
     DESCANSO_CURTO: {
-        id: "descansoCurto",
-        nome: "Descanso Curto",
+        id: "descanso-curto",
+        nome: "Descanso curto",
         frase: ["Que tal dar uma respirada?", "Faça uma pausa curta."],
         tempoInicialEmSegundos: 5,
     },
     DESCANSO_LONGO: {
-        id: "descansoLongo",
-        nome: "Descanso Longo",
-        frase: ["Hora de voltar à superficie.", "Faça uma pausa longa."],
+        id: "descanso-longo",
+        nome: "Descanso longo",
+        frase: ["Hora de voltar à superfície.", "Faça uma pausa longa."],
         tempoInicialEmSegundos: 15,
-    }
-}
+    },
+};
 
 export const useCronometroStore = create((set) => ({
     modoCronometro: MODO_CRONOMETRO.FOCO,
-    tempoInicialEmSegundos: MODO_CRONOMETRO.FOCO.tempoInicialEmSegundos,
+    tempoEmSegundos: MODO_CRONOMETRO.FOCO.tempoInicialEmSegundos,
 
     setModoCronometro: (novoModo) => {
         set({
             modoCronometro: novoModo,
-            tempoInicialEmSegundos: novoModo.tempoInicialEmSegundos,
-        })
+            tempoEmSegundos: novoModo.tempoInicialEmSegundos,
+        });
+    },
+
+    intervaloId: null,
+
+    iniciarCronometro: () => {
+        const novoId = setInterval(computarContagemRegressiva, 1000);
+
+        set({ intervaloId: novoId });
+    },
+}));
+
+function computarContagemRegressiva() {
+    const tempoAtual = useCronometroStore.getState().tempoEmSegundos;
+
+    if (tempoAtual > 0) {
+        decrementarTempo();
+    } else {
+        redefinirTempo();
     }
-}))
+}
+
+function decrementarTempo() {
+    useCronometroStore.setState((estado) => ({ tempoEmSegundos: estado.tempoEmSegundos - 1 }));
+}
+
+function redefinirTempo() {
+    useCronometroStore.setState((estado) => ({
+        tempoEmSegundos: estado.modoCronometro.tempoInicialEmSegundos,
+    }));
+}
